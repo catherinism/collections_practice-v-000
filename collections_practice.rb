6492f516c518dabc8bb3bc9279dededcd57429cc
Catherine Salazar <catcreates13@gmail.com>
2018-01-25 02:50:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,8 @@ def sum_array(integers)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| array[element] = index }
+  array.each_with_index.collect {|element| element[1] = array }
+  element << "s"
 
 
 end
